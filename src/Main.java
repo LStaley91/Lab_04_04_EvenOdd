b7a0004d164751681dc29258b4c6328a2bd94754
LStaley <lstaley9185@gmail.com>
2022-09-15 17:34:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 2;
+        if (numToExamine % 2 == 0)
+            System.out.println(numToExamine+" is an even number.");
+        else
+            System.out.println(numToExamine+" is an odd number.");
+    }
+}
